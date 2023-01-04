63224c3f50f0c24edba6380eb2bfed54dda7b14e
Luzmoreno782 <luz1010ms@gmail.com>
2023-01-04 19:09:44 -0300

    git commit https://github.com/Luzmaria94/crud22547.git echo "# crud22547" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Luzmaria94/crud22547.git git push -u origin main

---
+++ OneDrive/Documents/integrador base datos/integrador base datos.sql
@@ -0,0 +1,15 @@
+SELECT * FROM `integrador base datos`.`base de datos`;
+use db22547;
+select * FROM id;
+select max (id);
+select id as "codigo";
+start transaction;
+insert into nombre value (lina);
+commit;
+select distinct (id_provincia);
+
+
+
+
+
+
